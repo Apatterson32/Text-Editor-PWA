5eecc583cd355500e8ad6003cd8e5bececb6a44a
Alissa <pattersonal10@gmail.com>
2023-11-30 16:24:32 -0600

    src-sw.js and webpackconfig

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,5 +26,29 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+// asset caching
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new CacheFirst({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  }),
+);
+
+// variable for offline fallback
+const customStrategy = async ({ event }) => {
+  try {
+    return await pageCache.handle({ event });
+  } catch (error) {
+    return offlineFallback({ event });
+  }
+};
+
+registerRoute(
+  ({ request }) => request.mode === 'navigate',
+  customStrategy
+);
